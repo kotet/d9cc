f8723100d891408df913da5e2b21da93eafaa806
Kotet <kotet0418@gmail.com>
2018-10-14 17:56:10 +0900

    Add "+" and "-"
    [フォーマット指定子一覧](https://www.k-cube.co.jp/wakaba/server/format.html)
    [strtol](http://www9.plala.or.jp/sgwr-t/lib/strtol.html)

--- d9cc.d
+++ d9cc.d
@@ -9,10 +9,44 @@ int main(string[] args)
         return 1;
     }
 
+    size_t i;
+    string s = args[1];
+
     writeln(".intel_syntax noprefix"); // intel記法を使う
     writeln(".global main");
     writeln("main:");
-    writefln("  mov rax, %d", args[1].to!int);
+    writefln("  mov rax, %d", s.nextInt(i));
+
+    while (i < s.length) // Dの文字列はNull終端ではない
+    {
+        if (s[i] == '+')
+        {
+            i++;
+            writefln("  add rax, %d", s.nextInt(i));
+            continue;
+        }
+        if (s[i] == '-')
+        {
+            i++;
+            writefln("  sub rax, %d", s.nextInt(i));
+            continue;
+        }
+
+        stderr.writefln("Unexpected character: %s", s[i]);
+    }
+
     writeln("  ret");
     return 0;
+}
+
+/// strtolの代わり
+int nextInt(string s, ref size_t i)
+{
+    int result;
+    while (i < s.length && '0' <= s[i] && s[i] <= '9')
+    {
+        result = (result * 10) + (s[i] - '0');
+        i++;
+    }
+    return result;
 }
--- test.sh
+++ test.sh
@@ -9,7 +9,9 @@ try() {
     ./tmp
     actual="$?"
 
-    if [ "$actual" != "$expected" ]; then
+    if [ "$actual" == "$expected" ]; then
+        echo "$input => $actual"
+    else
         echo "$input expected, but got $actual"
         exit 1
     fi
@@ -17,6 +19,7 @@ try() {
 
 try 0 0
 try 42 42
+try 21 '5+20-4'
 
 echo "＿人人人＿"
 echo "＞　OK　＜"
