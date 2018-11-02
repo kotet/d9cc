2a0482fc08d9081a322a3e7165649430c89db7a8
Kotet <kotet0418@gmail.com>
2018-11-02 15:17:56 +0900

    Do not use struct assignment

--- sema.d
+++ sema.d
@@ -44,12 +44,12 @@ struct Variable
 
 // decay == trueならIDENTIFIERノードをADDRESSノードに書き換える。
 // decay == falseになるのは arr = {1,2,3}みたいなときだと思う
-void walk(Node* node, bool decay, ref Variable[string] vars, ref size_t stacksize)
+Node* walk(Node* node, bool decay, ref Variable[string] vars, ref size_t stacksize)
 {
     with (NodeType) switch (node.op)
     {
     case NUM:
-        return;
+        return node;
     case IDENTIFIER:
         if (!(node.name in vars))
         {
@@ -60,25 +60,21 @@ void walk(Node* node, bool decay, ref Variable[string] vars, ref size_t stacksiz
 
         if (decay && vars[node.name].type.type == TypeName.ARRAY)
         {
-            *node = () {
+            return () {
                 Node* n = new Node();
-                Node* _node = new Node();
-                *_node = *node;
-
                 n.op = NodeType.ADDRESS;
                 Type* t = new Type(TypeName.POINTER);
                 t.pointer_of = vars[node.name].type.array_of;
                 n.type = t;
-                n.expr = _node;
-                return *n;
+                n.expr = node;
+                return n;
             }();
         }
         else
         {
             node.type = vars[node.name].type;
+            return node;
         }
-
-        return;
     case VARIABLE_DEFINITION:
         stacksize += size_of(*node.type);
         node.offset = stacksize;
@@ -90,27 +86,27 @@ void walk(Node* node, bool decay, ref Variable[string] vars, ref size_t stacksiz
 
         if (node.initalize)
         {
-            walk(node.initalize, true, vars, stacksize);
+            node.initalize = walk(node.initalize, true, vars, stacksize);
         }
-        return;
+        return node;
     case IF:
-        walk(node.cond, true, vars, stacksize);
-        walk(node.then, true, vars, stacksize);
+        node.cond = walk(node.cond, true, vars, stacksize);
+        node.then = walk(node.then, true, vars, stacksize);
         if (node.els)
         {
-            walk(node.els, true, vars, stacksize);
+            node.els = walk(node.els, true, vars, stacksize);
         }
-        return;
+        return node;
     case FOR:
-        walk(node.initalize, true, vars, stacksize);
-        walk(node.cond, true, vars, stacksize);
-        walk(node.inc, true, vars, stacksize);
-        walk(node.bdy, true, vars, stacksize);
-        return;
+        node.initalize = walk(node.initalize, true, vars, stacksize);
+        node.cond = walk(node.cond, true, vars, stacksize);
+        node.inc = walk(node.inc, true, vars, stacksize);
+        node.bdy = walk(node.bdy, true, vars, stacksize);
+        return node;
     case ADD:
     case SUB:
-        walk(node.lhs, true, vars, stacksize);
-        walk(node.rhs, true, vars, stacksize);
+        node.lhs = walk(node.lhs, true, vars, stacksize);
+        node.rhs = walk(node.rhs, true, vars, stacksize);
         if (node.rhs.type.type == TypeName.POINTER)
         {
             // a - bがb - aになっちゃうけどいいんだろうか……
@@ -121,54 +117,54 @@ void walk(Node* node, bool decay, ref Variable[string] vars, ref size_t stacksiz
             error("Pointer %s pointer is not defined", cast(char) node.op);
         }
         node.type = node.lhs.type;
-        return;
+        return node;
     case MUL:
     case DIV:
     case LESS_THAN:
     case LOGICAL_OR:
     case LOGICAL_AND:
-        walk(node.lhs, true, vars, stacksize);
-        walk(node.rhs, true, vars, stacksize);
+        node.lhs = walk(node.lhs, true, vars, stacksize);
+        node.rhs = walk(node.rhs, true, vars, stacksize);
         node.type = node.lhs.type;
-        return;
+        return node;
     case ASSIGN:
-        walk(node.lhs, false, vars, stacksize);
-        walk(node.rhs, true, vars, stacksize);
+        node.lhs = walk(node.lhs, false, vars, stacksize);
+        node.rhs = walk(node.rhs, true, vars, stacksize);
         node.type = node.lhs.type;
-        return;
+        return node;
     case DEREFERENCE:
-        walk(node.expr, true, vars, stacksize);
+        node.expr = walk(node.expr, true, vars, stacksize);
         if (node.expr.type.type != TypeName.POINTER)
         {
             error("Operand must be a pointer");
         }
         node.type = node.expr.type.pointer_of;
-        return;
+        return node;
     case RETURN:
-        walk(node.expr, true, vars, stacksize);
+        node.expr = walk(node.expr, true, vars, stacksize);
         node.type = new Type(TypeName.INT);
-        return;
+        return node;
     case CALL:
-        foreach (ref arg; node.args)
-            walk(&arg, true, vars, stacksize);
+        foreach (i, ref arg; node.args)
+            node.args[i] = *walk(&arg, true, vars, stacksize);
         node.type = new Type(TypeName.INT);
-        return;
+        return node;
     case FUNCTION:
-        foreach (ref arg; node.args)
-            walk(&arg, true, vars, stacksize);
+        foreach (i, ref arg; node.args)
+            node.args[i] = *walk(&arg, true, vars, stacksize);
         walk(node.bdy, true, vars, stacksize);
-        return;
+        return node;
     case COMPOUND_STATEMENT:
-        foreach (ref stmt; node.statements)
-            walk(&stmt, true, vars, stacksize);
-        return;
+        foreach (i, ref stmt; node.statements)
+            node.statements[i] = *walk(&stmt, true, vars, stacksize);
+        return node;
     case EXPRESSION_STATEMENT:
-        walk(node.expr, true, vars, stacksize);
-        return;
+        node.expr = walk(node.expr, true, vars, stacksize);
+        return node;
     case ADDRESS:
-        walk(node.expr, true, vars, stacksize);
+        node.expr = walk(node.expr, true, vars, stacksize);
         node.type = node.expr.type.pointer_of;
-        return;
+        return node;
     default:
         error("Unknown node type: %s", node.op);
         assert(0);
